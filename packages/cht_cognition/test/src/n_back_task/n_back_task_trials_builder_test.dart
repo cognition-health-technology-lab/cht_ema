d0a2c9a3b428a190ec635d8129d95d787f781652
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2026-03-23 10:28:22 -0400

    refactor(cognition): rename match proportion in n-back

--- packages/cht_cognition/test/src/n_back_task/n_back_task_trials_builder_test.dart
+++ packages/cht_cognition/test/src/n_back_task/n_back_task_trials_builder_test.dart
@@ -7,11 +7,11 @@ void main() {
       'When given n < 1, throws an error',
       () {
         expect(
-          () => buildNBackTrials(n: 0, matchProbability: 0.5),
+          () => buildNBackTrials(n: 0, matchProportion: 0.5),
           throwsArgumentError,
         );
         expect(
-          () => buildNBackTrials(n: -1, matchProbability: 0.5),
+          () => buildNBackTrials(n: -1, matchProportion: 0.5),
           throwsArgumentError,
         );
       },
@@ -21,7 +21,7 @@ void main() {
       'matches = 3, creates a list of 30 trials with the correct match '
       'proportion and no more than 3 consecutive matches',
       () {
-        final trials = buildNBackTrials(n: 30, matchProbability: 0.3);
+        final trials = buildNBackTrials(n: 30, matchProportion: 0.3);
         expect(trials.length, 30);
 
         var matchCount = 0;
