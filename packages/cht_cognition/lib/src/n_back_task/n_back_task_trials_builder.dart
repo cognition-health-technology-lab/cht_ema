c9f94f08be408b109cfc48365c8a710d5c39b673
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2026-03-23 11:12:39 -0400

    WIP(cognition): implement n-back trials builder

--- packages/cht_cognition/lib/src/n_back_task/n_back_task_trials_builder.dart
+++ packages/cht_cognition/lib/src/n_back_task/n_back_task_trials_builder.dart
@@ -2,18 +2,58 @@ import 'package:cht_cognition/src/core/trials/randomization_type.dart';
 import 'package:cht_cognition/src/core/trials/trial.dart';
 import 'package:cht_cognition/src/n_back_task/n_back_stim.dart';
 
-List<Trial<NBackStim>> buildNBackTrials({
-  required int n,
-  required double matchProportion,
-  RandomizationType randomizationType = RandomizationType.full,
-  int? seed,
-}) {
+List<Trial<NBackStim>> buildNBackTrials(
+  //   {
+  //   required int n,
+  //   required double matchProportion,
+  //   RandomizationType randomizationType = RandomizationType.full,
+  //   int? seed,
+  // }
+) {
+  const n = 30;
+  const matchProportion = 0.3;
+  const maxConsecutiveMatches = 3;
+
   if (n < 1) {
     throw ArgumentError('n must be greater than 0');
   }
-  return <Trial<NBackStim>>[
-    const Trial<NBackStim>(stim: NBackStim.a),
-    const Trial<NBackStim>(stim: NBackStim.b),
-    const Trial<NBackStim>(stim: NBackStim.c),
+
+  /// calculate counts by type
+  const matchCount = n * matchProportion;
+  const nonMatchCount = n - matchCount;
+
+  /// plan next steps
+
+  return const <Trial<NBackStim>>[
+    Trial<NBackStim>(stim: NBackStim.a),
+    Trial<NBackStim>(stim: NBackStim.a),
+    Trial<NBackStim>(stim: NBackStim.a),
+    Trial<NBackStim>(stim: NBackStim.a),
+    Trial<NBackStim>(stim: NBackStim.b),
+    Trial<NBackStim>(stim: NBackStim.b),
+    Trial<NBackStim>(stim: NBackStim.b),
+    Trial<NBackStim>(stim: NBackStim.b),
+    Trial<NBackStim>(stim: NBackStim.c),
+    Trial<NBackStim>(stim: NBackStim.a),
+    Trial<NBackStim>(stim: NBackStim.b),
+    Trial<NBackStim>(stim: NBackStim.c),
+    Trial<NBackStim>(stim: NBackStim.a),
+    Trial<NBackStim>(stim: NBackStim.b),
+    Trial<NBackStim>(stim: NBackStim.c),
+    Trial<NBackStim>(stim: NBackStim.a),
+    Trial<NBackStim>(stim: NBackStim.b),
+    Trial<NBackStim>(stim: NBackStim.c),
+    Trial<NBackStim>(stim: NBackStim.a),
+    Trial<NBackStim>(stim: NBackStim.b),
+    Trial<NBackStim>(stim: NBackStim.c),
+    Trial<NBackStim>(stim: NBackStim.a),
+    Trial<NBackStim>(stim: NBackStim.b),
+    Trial<NBackStim>(stim: NBackStim.c),
+    Trial<NBackStim>(stim: NBackStim.a),
+    Trial<NBackStim>(stim: NBackStim.b),
+    Trial<NBackStim>(stim: NBackStim.c),
+    Trial<NBackStim>(stim: NBackStim.c),
+    Trial<NBackStim>(stim: NBackStim.c),
+    Trial<NBackStim>(stim: NBackStim.c),
   ];
 }
