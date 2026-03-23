e2a0e3327cf40a5803a9ae3df4695d9d3f22111a
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2026-03-23 10:43:59 -0400

    test(cognition): extract utility func to count matches

--- packages/cht_cognition/test/src/n_back_task/n_back_task_trials_builder_test.dart
+++ packages/cht_cognition/test/src/n_back_task/n_back_task_trials_builder_test.dart
@@ -1,3 +1,5 @@
+import 'package:cht_cognition/src/core/trials/trial.dart';
+import 'package:cht_cognition/src/n_back_task/n_back_stim.dart';
 import 'package:cht_cognition/src/n_back_task/n_back_task_trials_builder.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -22,16 +24,24 @@ void main() {
       'proportion and no more than 3 consecutive matches',
       () {
         final trials = buildNBackTrials(n: 30, matchProportion: 0.3);
+
+        /// test length
         expect(trials.length, 30);
 
-        var matchCount = 0;
-        for (var i = 1; i < trials.length; i++) {
-          if (trials[i].stim == trials[i - 1].stim) {
-            matchCount++;
-          }
-        }
-        expect(matchCount, 9);
+        /// test match proportion
+        final adjacentMatchCount = _countAdjacentMatches(trials);
+        expect(adjacentMatchCount, 9);
       },
     );
   });
+}
+
+int _countAdjacentMatches(List<Trial<NBackStim>> trials) {
+  var count = 0;
+  for (var i = 1; i < trials.length; i++) {
+    if (trials[i].stim == trials[i - 1].stim) {
+      count++;
+    }
+  }
+  return count;
 }
