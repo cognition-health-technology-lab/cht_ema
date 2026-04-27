ab411056a15db02e3a76c1f1e94415bca6c056a3
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2026-04-27 16:37:38 -0400

    feat(mhealthgoal): impl getting permission for pedometer

--- apps/mhealthgoal/lib/src/core/physical_acitivity/pedometer/pedometer_service.dart
+++ apps/mhealthgoal/lib/src/core/physical_acitivity/pedometer/pedometer_service.dart
@@ -1,7 +1,24 @@
+import 'dart:io' show Platform;
+
 import 'package:pedometer/pedometer.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class PedometerService {
   final Pedometer _pedometer;
+  bool permissionGranted = false;
 
   PedometerService({required Pedometer pedometer}) : _pedometer = pedometer;
+
+  /// Asks the user for permission to access activity recognition on Android,
+  /// if it has not been granted. It has no effect on iOS because it is
+  /// handled automatically by ios.
+  Future<void> askPermission() async {
+    if (Platform.isIOS) {
+      permissionGranted = true;
+    } else if (Platform.isAndroid) {
+      permissionGranted = await Permission.activityRecognition
+          .request()
+          .isGranted;
+    }
+  }
 }
