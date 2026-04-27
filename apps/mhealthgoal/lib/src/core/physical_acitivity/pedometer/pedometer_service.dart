ac3cfdff80478ef918a148ba01592f5ef521b60b
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2026-04-27 10:34:58 -0400

    feat(mhealthgoal): impl base pedometer service

---
+++ apps/mhealthgoal/lib/src/core/physical_acitivity/pedometer/pedometer_service.dart
@@ -0,0 +1,7 @@
+import 'package:pedometer/pedometer.dart';
+
+class PedometerService {
+  final Pedometer _pedometer;
+
+  PedometerService({required Pedometer pedometer}) : _pedometer = pedometer;
+}
