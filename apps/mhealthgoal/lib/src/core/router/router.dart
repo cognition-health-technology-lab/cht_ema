daa0a4e4086748e538f6eab8bc3e92145ef46707
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2026-05-26 12:32:42 -0400

    improve(mhealthgoal): define method for navigating to home page

--- apps/mhealthgoal/lib/src/core/router/router.dart
+++ apps/mhealthgoal/lib/src/core/router/router.dart
@@ -1,9 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mhealthgoal/src/features/home/presentation/home_page.dart';
 
+/// Domain-level interface to the app's router.
+///
+/// The app should only depend on this class instead of external
+/// routing dependencies.
 class AppRouter {
   final router = GoRouter(routes: _routes, initialLocation: '/');
   final routes = _routes;
+
+  void navigateToHome(BuildContext context) => context.go('/home');
 }
 
 final _routes = <GoRoute>[
