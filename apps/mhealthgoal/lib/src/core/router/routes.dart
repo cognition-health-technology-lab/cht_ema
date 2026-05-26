1de220a5847cf8fcc2a3d88875faf2f858f978f1
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2026-05-26 12:10:22 -0400

    improve(mhealthgoal): add name to home route

--- apps/mhealthgoal/lib/src/core/router/routes.dart
+++ apps/mhealthgoal/lib/src/core/router/routes.dart
@@ -2,5 +2,9 @@ import 'package:go_router/go_router.dart';
 import 'package:mhealthgoal/src/features/home/presentation/home_page.dart';
 
 final routes = <GoRoute>[
-  GoRoute(path: '/', builder: (context, state) => const HomePage()),
+  GoRoute(
+    name: 'home',
+    path: '/',
+    builder: (context, state) => const HomePage(),
+  ),
 ];
