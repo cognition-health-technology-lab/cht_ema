aa41e9dccc5cd133d3a0f2f37ea250b941c98dde
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2026-05-26 12:22:32 -0400

    refactor(mhealthgoal): define app interface to router

--- apps/mhealthgoal/lib/src/app/app.dart
+++ apps/mhealthgoal/lib/src/app/app.dart
@@ -3,14 +3,17 @@ import 'package:mhealthgoal/src/core/router/router.dart';
 import 'package:mhealthgoal/src/l10n/generated/app_localizations.dart';
 
 class MHealthGoalApp extends StatelessWidget {
-  const MHealthGoalApp({super.key});
+  final AppRouter _appRouter;
+
+  const MHealthGoalApp({required AppRouter appRouter, super.key})
+    : _appRouter = appRouter;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      routerConfig: router,
+      routerConfig: _appRouter.router,
     );
   }
 }
--- apps/mhealthgoal/lib/src/core/router/router.dart
+++ apps/mhealthgoal/lib/src/core/router/router.dart
@@ -1,4 +1,15 @@
 import 'package:go_router/go_router.dart';
-import 'package:mhealthgoal/src/core/router/routes.dart';
+import 'package:mhealthgoal/src/features/home/presentation/home_page.dart';
 
-final router = GoRouter(routes: routes, initialLocation: '/');
+class AppRouter {
+  final router = GoRouter(routes: _routes, initialLocation: '/');
+  final routes = _routes;
+}
+
+final _routes = <GoRoute>[
+  GoRoute(
+    name: 'home',
+    path: '/',
+    builder: (context, state) => const HomePage(),
+  ),
+];
